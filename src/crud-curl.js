2be4021cf162c787358c1b2f60d1a49da657e078
pbarlar <pbarlar@g.educaand.es>
2025-10-16 08:49:36 +0200

    Function createStudent added in crud-curl.js

--- src/crud-curl.js
+++ src/crud-curl.js
@@ -5,3 +5,30 @@ import { exec } from 'child_process';
 const API_URL = process.env.API_URL || "http://localhost";
 const PORT=process.env.PORT || 4000;
 const BASE_URL=`${API_URL}:${PORT}`
+
+//FUNCION CREAR ESTUDIANTES
+export async function createStudent(studentData) {
+  const url = `${BASE_URL}/students`;
+  console.log(`📋 CREATE → POST ${url}`);
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(studentData),
+    });
+
+    const data = await response.json();
+    console.log("✅ Estudiante creado:", data);
+    return data;
+  } catch (error) {
+    console.error("❌ Error al crear estudiante:", error);
+  }
+}
+
+
+
+
+
+
+
